b73ce06f0d6657263a4f0de1d5e61161d5271b04
rktrail <antoine.deleplanque@hotmail.fr>
2014-05-05 14:32:24 +0200

    UserObject

--- Object/Utilisateur.php
+++ Object/Utilisateur.php
@@ -12,5 +12,53 @@
  * @author AntoineD
  */
 class Utilisateur {
-    //put your code here
+    private $_id;
+    private $_login;
+    private $_password;
+    private $_email;
+    private $_type;
+    public function get_id() {
+        return $this->_id;
+    }
+
+    public function get_login() {
+        return $this->_login;
+    }
+
+    public function get_password() {
+        return $this->_password;
+    }
+
+    public function get_email() {
+        return $this->_email;
+    }
+
+    public function set_login($_login) {
+        $this->_login = $_login;
+    }
+
+    public function set_email($_email) {
+        $this->_email = $_email;
+    }
+
+    function __construct($_id, $_login, $_password, $_email, $_type) {
+        $this->_id = $_id;
+        $this->_login = $_login;
+        $this->_password = $_password;
+        $this->_email = $_email;
+        $this->_type = $_type;
+    }
+    
+    public function GenererPassword(){
+        $characts   = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';   
+        $characts   .= '1234567890'; 
+        $newPassword = '';
+ 
+        for($i=0;$i < 6;$i++)
+        {
+             $newPassword .= substr($characts,rand()%(strlen($characts)),1);
+        }
+        $this->_password = $newPassword;
+    }
+
 }
